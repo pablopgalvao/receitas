d7589a37acd5b9ac519e6ad4fc30bb5ca512159b
Pablo Galvao <pablogalvao614@gmail.com>
2026-03-15 17:45:25 -0300

    feat: adicionado deeplink para lar do chef

--- scripts/api-generator.js
+++ scripts/api-generator.js
@@ -56,7 +56,7 @@ hexo.extend.generator.register('api-receitas', function(locals) {
     return {
       id: post._id,
       title: post.title,
-      url: baseUrl + rootPath + post.path, // 🔗 link absoluto do post
+      url: toAbsoluteUrl(rootPath + post.path), // 🔗 link absoluto do post
       image: postImage,
       images: images,
       videos: videos,
